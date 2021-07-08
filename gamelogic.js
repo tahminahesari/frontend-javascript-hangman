4056b49ebfb7677ee5c4225be74a49785f1df9a3
tahminahesari <hesari@live.com>
2021-07-08 20:39:16 +0200

    eerste test

--- gamelogic.js
+++ gamelogic.js
@@ -1,6 +1,22 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
-}
+  let string = "";
+  const array = word.split("");
+  console.log(array);
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    if (guesses.includes(array[i])) {
+      string += array[i] + " ";
+    } else string += "_ ";
+  }
+  console.log(string);
+  return string
+
+  // * Hoe kan ik iedere letter afzonderlijk bekijken
+  // * HOe kan ik automaitsch over alle letters van het woord luisteren
+  // * Zit deze letter in de guesses aaray, zo ja print letter zelf
+  // * anders underscore
+
+  }
 
 function isGameWon(word, guesses) {
   // WRITE ME
